bb99c805a1ef023a82e274cf07241e9c158efa50
LAB24_USER ON WS12 <L24X09W12@LabHCI.net>
2026-03-06 09:03:41 +0800

    feat: Spring Security dependency for BCrypt

---
+++ backend/medify/src/main/java/edu/cit/jaquez/medify/MedifyApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.jaquez.medify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedifyApplication.class, args);
+	}
+
+}
---
+++ backend/medify/src/test/java/edu/cit/jaquez/medify/MedifyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.jaquez.medify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
